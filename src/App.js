0b23183cd3e40e513ff50ec063f28e0324f8006e
Babrakoskei <babrakoskei@gmail.com>
2022-05-23 15:41:54 +0300

    node

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-;
+
 import Contact from "components/Contact";
 import Footer from "components/Footer";
 import Home from "components/Home";
-;
 import Portfolio from "components/Portfolio";
-
 import ScrollToTop from "components/ScrollToTop";
 import Services from "components/Services";
-
 import React from "react";
 import { motion } from "framer-motion";
 

035298ab4eab1d95576fd420a3e8053cb252bbb7
Babrakoskei <babrakoskei@gmail.com>
2022-05-23 16:45:25 +0300

    href

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Contact from "components/Contact";
 import Footer from "components/Footer";
 import Home from "components/Home";
 import Portfolio from "components/Portfolio";
-import ScrollToTop from "components/ScrollToTop";
+// import ScrollToTop from "components/ScrollToTop";
 import Services from "components/Services";
 import React from "react";
 import { motion } from "framer-motion";
@@ -11,7 +11,7 @@ import { motion } from "framer-motion";
 export default function App() {
   return (
     <motion.div initial="hidden" animate="show">
-      <ScrollToTop />
+      {/* <ScrollToTop /> */}
       <Home />
       <Services />
       <Portfolio />
